bc26522d0fb2f3ca53127b86fdad62e05a4882a9
Samuel Rain <niaertubber@gmail.com>
2025-07-27 18:39:24 +0700

    Update Model Question

--- sosiometry-app/app/Models/Question.php
+++ sosiometry-app/app/Models/Question.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['question_text', 'type', 'is_active'];
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 }
