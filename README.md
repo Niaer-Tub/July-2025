4362292c65c9d75ab408314f0ce67f79d16bf11e
Samuel Rain <niaertubber@gmail.com>
2025-07-28 11:24:49 +0700

    Make Sanctum

--- README.md
+++
@@ -1,4 +0,0 @@
-# July-2025
-Last Project July
-This is School Project 2025
-Free to use
--- sosiometry-app/app/Models/Response.php
+++ sosiometry-app/app/Models/Response.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Response extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['question_id', 'responder_id', 'subject_id', 'score'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function responder()
+    {
+        return $this->belongsTo(User::class, 'responder_id');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(User::class, 'subject_id');
+    }
 }
