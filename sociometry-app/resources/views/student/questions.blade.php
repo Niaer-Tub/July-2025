4118ce706018da913a0ac5d5fac7aa9196beb85a
Samuel Rain <niaertubber@gmail.com>
2025-08-03 21:36:46 +0700

    Fixing Submit

--- sociometry-app/resources/views/student/questions.blade.php
+++ sociometry-app/resources/views/student/questions.blade.php
@@ -24,9 +24,10 @@
             @endforeach
 
             <div class="mt-6">
-                <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+                <button type="submit" class="bg-blue-600 text-black px-4 py-2 rounded hover:bg-blue-700">
                     Kirim Jawaban
                 </button>
+
             </div>
         </form>
     </div>
