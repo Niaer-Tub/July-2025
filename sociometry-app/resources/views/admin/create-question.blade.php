0b36536a5c1916abae4e28ac4a50c264c0fffea6
Samuel Rain <niaertubber@gmail.com>
2025-08-05 08:44:11 +0700

    Debugging Edit

--- sociometry-app/resources/views/admin/create-question.blade.php
+++ sociometry-app/resources/views/admin/create-question.blade.php
@@ -6,7 +6,7 @@
             @csrf
             <label class="block mb-2">Question Text</label>
             <textarea name="text" required class="w-full border p-2 rounded">{{ old('text') }}</textarea>
-            <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Save</button>
+            <button class="mt-4 bg-blue-500 text-black px-4 py-2 rounded">Save</button>
         </form>
     </div>
 </x-app-layout>
