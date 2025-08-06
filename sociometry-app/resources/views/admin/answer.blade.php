be0f11fcb3477ebd3f83f8feab4a45b49c76de49
Samuel Rain <niaertubber@gmail.com>
2025-08-06 10:41:17 +0700

    Debug Answers

--- sociometry-app/resources/views/admin/answer.blade.php
+++ sociometry-app/resources/views/admin/answer.blade.php
@@ -1,18 +1,28 @@
-<x-app-layout>
-    <x-slot name="header">All Student Answers</x-slot>
+<h2 class="text-xl font-semibold mt-10 mb-4">Jawaban Siswa</h2>
 
-    <div class="p-6">
-        @foreach($answers as $answer)
-            <div class="border p-3 mb-3">
-                <p><strong>{{ $answer->user->name }}</strong> answered:</p>
-                <p>Question: {{ $answer->question->text }}</p>
-                <p>Selected Friends:</p>
-                <ul class="list-disc list-inside">
-                    @foreach($answer->selectedFriends as $friend)
-                        <li>{{ $friend->name }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endforeach
-    </div>
-</x-app-layout>
+@if($answers->isEmpty())
+    <p class="text-gray-500">Belum ada jawaban.</p>
+@else
+    <table class="w-full border">
+        <thead>
+            <tr class="bg-gray-100 text-left">
+                <th class="p-2 border">Nama Siswa</th>
+                <th class="p-2 border">Pertanyaan</th>
+                <th class="p-2 border">Jawaban</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($answers as $answer)
+                <tr>
+                    <td class="p-2 border">{{ $answer->user->name ?? '-' }}</td>
+                    <td class="p-2 border">{{ $answer->question->text ?? '-' }}</td>
+                    <td class="p-2 border">
+                        @foreach(json_decode($answer->selected_names, true) as $name)
+                            <span class="inline-block bg-gray-200 px-2 py-1 rounded text-sm mr-1">{{ $name }}</span>
+                        @endforeach
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endif
